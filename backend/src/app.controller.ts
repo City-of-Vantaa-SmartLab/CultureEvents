5060fafd83e7b6c0c4438250a5d070b6dcceac73
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-06-15 17:24:05 +0300

    Serving static files from server

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+export const ROUTE_PREFIX = 'api';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  root(): string {
+  root(@Res() response): void {
+    // the homepage will load our index.html which contains React logic
+    response.sendFile('index.html');
+  }
+
+  @Get(ROUTE_PREFIX)
+  serverApi(): string {
+    // Some api data, can be an object also
     return this.appService.root();
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 require('dotenv').config();
+const path = require('path');
+
 const PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useStaticAssets(path.resolve(__dirname + '/../public'));
   await app.listen(PORT);
 }
 bootstrap();
