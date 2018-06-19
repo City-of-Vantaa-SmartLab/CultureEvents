ac92846d1fc705ccc82157d8e9799bc873b104cd
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-06-19 11:03:07 +0300

    Add react router. Change relax some rules

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
 import Button from 'antd/lib/button';
 import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default class App extends React.Component {
   public render() {
     return (
-      <section>
-        <h1>This is a react component</h1>
-        <Button type="primary"> HELLO </Button>
-      </section>
+      <Router>
+        <Switch>
+          <Route
+            path="/producer"
+            render={props => (
+              <section>
+                <h1>ProducerUI</h1>
+                <Button type="primary">HELLO</Button>
+              </section>
+            )}
+          />
+          <Route render={props => <h1>ConsumerUI</h1>} />
+        </Switch>
+      </Router>
     );
   }
 }
