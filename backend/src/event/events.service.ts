272a2bf0622963ed06fe33a3e7e6e257f4719dbd
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-07-18 10:36:23 +0300

    Changed age_group_limit as an array

--- backend/src/event/events.service.ts
+++ backend/src/event/events.service.ts
@@ -13,8 +13,12 @@ export class EventsService {
   ) {}
 
   async createEvent(event: EventsDto) {
-    const response = await this.eventRepository.save(event);
-    return response;
+    const event_to_save = {
+      ...event,
+      age_group_limit: event.age_group_limit.join(','),
+    };
+    const response = await this.eventRepository.save(event_to_save);
+    return this.mapSingleEventForAPI(response);
   }
 
   async deleteEvent(id: number) {
@@ -25,22 +29,45 @@ export class EventsService {
   async updateEvent(id: number, event: EventsDto) {
     const dbEvent = await this.eventRepository.findOne(id);
     if (dbEvent) {
-      await this.eventRepository.update(id, event);
-      return await this.eventRepository.findOne(id, {
+      const event_to_update = {
+        ...event,
+        age_group_limit: event.age_group_limit.join(','),
+      };
+      await this.eventRepository.update(id, event_to_update);
+      const updatedEvent = await this.eventRepository.findOne(id, {
         relations: ['ticket_catalog'],
       });
+      return this.mapSingleEventForAPI(updatedEvent);
     } else {
       return null;
     }
   }
 
-  async findAll(): Promise<Events[]> {
-    return await this.eventRepository.find({ relations: ['ticket_catalog'] });
+  async findAll(): Promise<EventsDto[]> {
+    const events = await this.eventRepository.find({
+      relations: ['ticket_catalog'],
+    });
+    return this.mapEventsForAPI(events);
   }
 
   async findOneById(id: number) {
-    return await this.eventRepository.findOne(id, {
+    const event = await this.eventRepository.findOne(id, {
       relations: ['ticket_catalog'],
     });
+    return this.mapSingleEventForAPI(event);
+  }
+
+  private mapEventsForAPI(events: Events[]) {
+    return events.map(event => ({
+      ...event,
+      age_group_limit: event.age_group_limit.split(','),
+    }));
+  }
+
+  private mapSingleEventForAPI(event: Events) {
+    return {
+      ...event,
+      age_group_limit: event.age_group_limit.split(','),
+    };
   }
 }
