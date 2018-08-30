cda316597882daab9a6c5c9b329c67f70ddf5ee5
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-08-30 14:00:44 +0300

    Added config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,10 +11,21 @@ import { PaymentModule } from 'payments/payment.module';
 import { SeedModule } from 'seed-db/seed.module';
 import { FileUploadModule } from 'fileupload/fileupload.module';
 import { FrontendMiddleware } from 'middleware/frontend.middleware';
-
+import { ConfigService } from './config/config.service';
+const configService = new ConfigService();
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      'type': 'postgres',
+      'host': configService.databaseHost,
+      'port': configService.databasePort,
+      'username': configService.databaseUsername,
+      'password': configService.databasePassword,
+      'database': configService.databaseName,
+      'entities': [`${configService.environment === 'local' ? 'src' : 'dist'}/**/**.entity{.ts,.js}`],
+      'synchronize': true
+    }
+    ),
     EventsModule,
     ReservationsModule,
     UserModule,
