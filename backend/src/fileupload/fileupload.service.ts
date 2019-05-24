346ccfedd6ed0362e3722d624817762cd751ad42
Nhu Minh Kha Nguyen <kha.nguyen@gofore.com>
2019-05-24 11:14:44 +0300

    Add image resize to reduce loading time

--- backend/src/fileupload/fileupload.service.ts
+++ backend/src/fileupload/fileupload.service.ts
@@ -1,47 +1,51 @@
-import { Req, Res, Injectable } from '@nestjs/common';
+import {Req, Res, Injectable} from '@nestjs/common';
 import * as multer from 'multer';
 import * as AWS from 'aws-sdk';
-import * as multerS3 from 'multer-s3';
+import s3Storage = require("multer-sharp-s3");
 
 const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME || 'vantaa-culture-events';
 const s3 = new AWS.S3();
 
 @Injectable()
 export class FileUploadService {
-  constructor() {
-    try {
-      if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
-        AWS.config.update({
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        });
-      }
-    } catch (error) {
-      console.error(`Failed to set AWS Config ${error}`);
-    }
-  }
-
-  async fileupload(@Req() req, @Res() res) {
-    try {
-      this.upload(req, res, function (error) {
-        if (error) {
-          return res.status(404).json(`Failed to upload image file: ${error}`);
+    constructor() {
+        try {
+            if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+                AWS.config.update({
+                    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                });
+            }
+        } catch (error) {
+            console.error(`Failed to set AWS Config ${error}`);
         }
-        return res.status(201).json(req.files[0].location);
-      });
-    } catch (error) {
-      return res.status(500).json(`Failed to upload image file: ${error}`);
     }
-  }
 
-  upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: AWS_S3_BUCKET_NAME,
-      acl: 'public-read',
-      key: function (_, file, cb) {
-        cb(null, `${Date.now().toString()} - ${file.originalname}`);
-      },
-    }),
-  }).array('upload', 1);
+    async fileupload(@Req() req, @Res() res) {
+        try {
+            this.upload(req, res, function (error) {
+                if (error) {
+                    return res.status(404).json(`Failed to upload image file: ${error}`);
+                }
+                return res.status(201).json(req.files[0].Location);
+            });
+        } catch (error) {
+            return res.status(500).json(`Failed to upload image file: ${error}`);
+        }
+    }
+
+    upload = multer({
+        storage: s3Storage({
+            s3: s3,
+            Bucket: AWS_S3_BUCKET_NAME,
+            ACL: 'public-read',
+            Key: function (_, file, cb) {
+                cb(null, `${Date.now().toString()} - ${file.originalname}`);
+            },
+            resize: {
+                width: 600,
+                height: 400,
+            },
+        }),
+    }).array('upload', 1);
 }
