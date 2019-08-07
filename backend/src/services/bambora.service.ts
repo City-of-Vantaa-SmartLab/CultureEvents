eefa1c6db273ef8db355ea6bfd2db5e6361202ff
Shamnad Shaji <shamnad.shaji@gofore.com>
2019-08-07 16:35:54 +0300

    test commit

--- backend/src/services/bambora.service.ts
+++ backend/src/services/bambora.service.ts
@@ -1,22 +1,35 @@
 import * as crypto from 'crypto';
 
-const payment_return_url = process.env.PAYMENT_RETURN_URL;
-const payemnt_notify_url = process.env.PAYMENT_NOTIFY_URL;
+const payment_return_url = 'http://kulttuuriliput-dev.eu-west-1.elasticbeanstalk.com/api/payments/payment-return';
+const payemnt_notify_url = 'http://kulttuuriliput-dev.eu-west-1.elasticbeanstalk.com/api/payments/payment-notify';
 
-const secret = process.env.BAMBORA_SECRET_KEY || 'SECRET_KEY';
-const apiKey = process.env.BAMBORA_API_KEY || 'API_KEY';
+const secret = '1ea00e5acc49440dfc514ababb3252c7';
+const apiKey = '2992d674acd5b09aed5063180c4c87cbdbce';
 
-const bamboraProductID = process.env.BAMBORA_PRODUCT_ID;
-const bamboraProductTitle = process.env.BAMBORA_PRODUCT_TITLE;
+const bamboraProductID = '327554_163330200_M0';
+const bamboraProductTitle = 'Saldovaraus Jumppaliput-sovellukseen';
 
 const BAMBORA_TAX = Number(process.env.BAMBORA_TAX) || 0;
 
 const VANTA_ORDER_PREFIX = process.env.VANTA_ORDER_PREFIX || 'vantaa-order-';
 
 export class BamboraService {
+
   createPaymentModel(paymentModel) {
+    console.log('process.env', process.env);
+    console.log('BAMBORA_API_KEY', process.env.BAMBORA_API_KEY);
+    console.log('BAMBORA_PRODUCT_ID', process.env.BAMBORA_PRODUCT_ID);
+    console.log('BAMBORA_PRODUCT_TITLE', process.env.BAMBORA_PRODUCT_TITLE);
+    console.log('BAMBORA_SECRET_KEY', process.env.BAMBORA_SECRET_KEY);
+    console.log('PAYMENT_NOTIFY_URL', process.env.PAYMENT_NOTIFY_URL);
+    console.log('PAYMENT_RETURN_URL', process.env.PAYMENT_RETURN_URL);
     const orderNumber = VANTA_ORDER_PREFIX + Date.now();
+    console.log('orderNumber', orderNumber);
+    console.log('apiKey', apiKey);
+    console.log('secret', secret);
+
     const message = apiKey + '|' + orderNumber;
+    console.log('message', message);
     const authCode = crypto
       .createHmac('sha256', secret)
       .update(message)
